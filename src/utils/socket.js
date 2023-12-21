6d06ada673e0e0333dd53adcc70ae6499445b718
techuser <techuser@gmail.com>
2023-12-21 16:30:14 +0530

    push the code

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client'
 
-export const BASEURL = process.env.NEXT_PUBLIC_API_ENDPOINT
+// export const BASEURL = process.env.NEXT_PUBLIC_API_ENDPOINT
+export const BASEURL = 'https://winstar-admin-backend.onrender.com'
 
-console.log('BASE_URL::', BASE_URL)
+console.log('BASEURL::', BASEURL)
 
 export const socket = io(BASEURL)
